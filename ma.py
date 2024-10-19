cbb7eb8d34e1e50cd2a91947b93b9cad4d19f88d
Sharoobi <nooraddinallddool@gmail.com>
2024-10-19 09:53:33 +0300

    first commit 2

---
+++ ma.py
@@ -0,0 +1,111 @@
+
+import random
+import cv2
+import cvzone
+from cvzone.HandTrackingModule import HandDetector
+import time
+import pygame
+
+pygame.mixer.init()
+
+# Load sound files
+sound_win = pygame.mixer.Sound("Resources/win.mp3")
+sound_lose = pygame.mixer.Sound("Resources/lose.mp3")
+sound_start = pygame.mixer.Sound("Resources/start.mp3")
+sound_tie = pygame.mixer.Sound("Resources/tie.mp3")
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+detector = HandDetector(maxHands=1)
+
+timer = 0
+stateResult = False
+startGame = False
+scores = [0, 0]
+initialTime = 0
+
+while True:
+    imgBG = cv2.imread("Resources/BG.png")
+    success, img = cap.read()
+
+    imgScaled = cv2.resize(img, (0, 0), None, 0.875, 0.875)
+    imgScaled = imgScaled[:, 80:480]
+
+    # Find Hands
+    hands, img = detector.findHands(imgScaled)  # with draw
+
+    if startGame:
+        if stateResult is False:
+            timer = time.time() - initialTime
+            cv2.putText(imgBG, str(int(timer)), (605, 435), cv2.FONT_HERSHEY_PLAIN, 6, (255, 0, 255), 4)
+
+            if timer > 3:
+                stateResult = True
+                timer = 0
+
+                if hands:
+                    playerMove = None
+                    hand = hands[0]
+                    fingers = detector.fingersUp(hand)
+                    if fingers == [0, 0, 0, 0, 0]:
+                        playerMove = 1
+                    if fingers == [1, 1, 1, 1, 1]:
+                        playerMove = 2
+                    if fingers == [0, 1, 1, 0, 0]:
+                        playerMove = 3
+
+                    randomNumber = random.randint(1, 3)
+                    imgAI = cv2.imread(f'Resources/{randomNumber}.png', cv2.IMREAD_UNCHANGED)
+                    imgBG = cvzone.overlayPNG(imgBG, imgAI, (149, 310))
+
+                    # Player Wins
+                    if (playerMove == 1 and randomNumber == 3) or \
+                            (playerMove == 2 and randomNumber == 1) or \
+                            (playerMove == 3 and randomNumber == 2):
+                        scores[1] += 1
+                        sound_win.play()
+
+                    # AI Wins
+                    elif (playerMove == 3 and randomNumber == 1) or \
+                            (playerMove == 1 and randomNumber == 2) or \
+                            (playerMove == 2 and randomNumber == 3):
+                        scores[0] += 1
+                        sound_lose.play()
+
+                    # Tie
+                    else:
+                        sound_tie.play()
+
+    imgBG[234:654, 795:1195] = imgScaled
+
+    if stateResult:
+        imgBG = cvzone.overlayPNG(imgBG, imgAI, (149, 310))
+
+    cv2.putText(imgBG, str(scores[0]), (410, 215), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 6)
+    cv2.putText(imgBG, str(scores[1]), (1112, 215), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 6)
+
+    cv2.imshow("BG", imgBG)
+
+    key = cv2.waitKey(1)
+    if key == ord('s'):
+        startGame = True
+        initialTime = time.time()
+        stateResult = False
+        sound_start.play()
+    elif key == ord('r') and stateResult:
+        startGame = True
+        initialTime = time.time()
+        stateResult = False
+        scores = [0, 0]  # Reset scores
+        imgBG = cv2.imread("Resources/BG.png")
+    elif key == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+# Release the camera and close all OpenCV windows
+
